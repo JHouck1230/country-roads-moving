4ae0e0fb28034058c2fc5e6fbb9af37dfd219715
Josh Houck <Josh123090@gmail.com>
2016-05-17 12:18:48 -0700

    v3 carousel and 3 views

--- client/js/module.js
+++ client/js/module.js
@@ -4,26 +4,26 @@ var app = angular.module('cormo', ['ui.router', 'ui.bootstrap', 'firebase', 'ngT
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-  // .state('home', {
-  //   url: '/',
-  //   templateUrl: '/html/home.html',
-  //   // controller: 'homeCtrl'
-  // })
+  .state('home', {
+    url: '/',
+    templateUrl: '/html/home.html',
+    controller: 'homeCtrl'
+  })
   .state('about', {
     url: '/about',
     templateUrl: '/html/about.html',
     // controller: 'aboutCtrl'
   })
-  .state('rates', {
-    url: '/rates',
-    templateUrl: '/html/rates.html',
-    // controller: 'ratesCtrl'
-  })
-  .state('calendar', {
-    url: '/calendar',
-    templateUrl: '/html/calendar.html',
-    // controller: 'calendarCtrl'
-  })
+  // .state('rates', {
+  //   url: '/rates',
+  //   templateUrl: '/html/rates.html',
+  //   // controller: 'ratesCtrl'
+  // })
+  // .state('calendar', {
+  //   url: '/calendar',
+  //   templateUrl: '/html/calendar.html',
+  //   // controller: 'calendarCtrl'
+  // })
   .state('contact', {
     url: '/contact',
     templateUrl: '/html/contact.html',
@@ -34,7 +34,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
     templateUrl: '/html/login.html',
     controller: 'loginCtrl'
   })
-  $urlRouterProvider.otherwise('/about');
+  $urlRouterProvider.otherwise('/');
 })
 
 app.constant('FB_URL', 'https://country-roads-moving.firebaseio.com/');
