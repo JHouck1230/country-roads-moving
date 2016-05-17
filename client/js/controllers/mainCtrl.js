994d0f3767948aecabbd571854e9aaab18213e46
Josh Houck <Josh123090@gmail.com>
2016-05-16 23:02:03 -0700

    add truck scroll progress

--- client/js/controllers/mainCtrl.js
+++ client/js/controllers/mainCtrl.js
@@ -2,6 +2,23 @@
 
 var app = angular.module('cormo');
 
-app.controller('mainCtrl', function($scope) {
-  console.log('mainCtrl');
+app.controller('mainCtrl', function($scope, $document, $window) {
+  this.scrollPos = 0;
+  $scope.forward = true;
+  let lastPos = 0;
+  window.onscroll = angular.bind(this, function(){
+    $scope.position = document.body.scrollTop || document.documentElement.scrollTop || 0;
+    $scope.max = $document.height() - $(window).height();
+    if(lastPos < $scope.position) {
+      $scope.forward = true;
+    } else {
+      $scope.forward = false;
+    }
+    $('.truckImg').css({
+      left: ($scope.position / $scope.max) * 100 + '%'
+    });
+    lastPos = $scope.position;
+    $scope.$digest();
+  });
+
 })
