6e70bf2794e9854426910e9ac4d537fa57cc87a9
Josh Houck <Josh123090@gmail.com>
2016-05-19 14:25:18 -0700

    add email capability to server

--- client/js/controllers/contactCtrl.js
+++ client/js/controllers/contactCtrl.js
@@ -5,5 +5,7 @@ var app = angular.module('cormo');
 app.controller('contactCtrl', function($scope, MailService) {
   $scope.sendMail = function() {
     MailService.sendMail($scope.customer)
+    .then(res => console.log("message sent! res: ", res.data),
+          err => console.error("Error: ", err))
   }
 })
--- routes/messages.js
+++ routes/messages.js
@@ -2,9 +2,46 @@
 
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+// var xoauth2 = require('xoauth2');
+
+var smtpConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: 'josh123090@gmail.com',
+        pass: process.env.GMAIL_PASS
+    }
+};
+
+var transporter = nodemailer.createTransport(smtpConfig);
 
 router.post('/', function(req, res) {
   console.log('req.body: ', req.body);
+  var mailOptions = {
+      to: 'josh123090@gmail.com',
+      subject: 'CRMO Message',
+      text: `Jake,
+
+Someone left a message for you on your website:
+
+    Customer Name:  ${req.body.name}
+    Phone Number:   ${req.body.phone}
+    Email Address:  ${req.body.email}
+    Message:        ${req.body.message}
+
+Cheers,
+Country Roads Moving Company`,
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        res.send(error);
+    }
+    console.log('Message sent: ' + info.response);
+    res.send(info.response)
+  });
 });
 
 module.exports = router;
